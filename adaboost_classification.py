b48e5d5e1c4ab530da1cfba8c5fa4ac785d5adeb
Vishnu Mohan <65121365+Vishnu-vm@users.noreply.github.com>
2020-09-05 11:17:50 +0530

    Add files via upload

---
+++ adaboost_classification.py
@@ -0,0 +1,22 @@
+# AdaBoost Classification
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import AdaBoostClassifier
+filename = '/Volumes/Data/Course Content/DS content/Tools/Python _Code/10.Bagging and Boosting/pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(filename, names=names)
+array = data.values
+
+X = array[:,0:8]
+Y = array[:,8]
+num_trees = 10
+seed=7
+kfold = KFold(n_splits=10, random_state=seed)
+model = AdaBoostClassifier(n_estimators=num_trees, random_state=seed)
+results = cross_val_score(model, X, Y, cv=kfold)
+print(results.mean())
+
+
+
+
