8ccc0e24138cee83d600f828d24c864e42f524c2
Vishnu Mohan <65121365+Vishnu-vm@users.noreply.github.com>
2020-09-08 19:41:02 +0530

    Add files via upload

---
+++ grid_search_new.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 12 08:02:55 2020
+
+@author: srinivasgurrala
+"""
+
+# Grid Search for Algorithm Tuning
+import numpy
+from pandas import read_csv
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+filename = '/Volumes/Data/Course Content/DS content/Tools/Python _Code/12.Grid search/pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(filename, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+
+n_neighbors = numpy.array([2,3,4,5,6,7,8,9,15])
+param_grid = dict(n_neighbors=n_neighbors)
+model = KNeighborsClassifier()
+grid = GridSearchCV(estimator=model, param_grid=param_grid)
+grid.fit(X, Y)
+print(grid.best_score_)
+print(grid.best_params_)
