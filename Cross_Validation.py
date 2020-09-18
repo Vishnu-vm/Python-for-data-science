a47495de1b875977385ceae1bb6e6c333dd2546f
Vishnu Mohan <65121365+Vishnu-vm@users.noreply.github.com>
2020-09-18 12:01:24 +0530

    Add files via upload

---
+++ Cross_Validation.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 10:48:33 2020
+
+@author: Sudheesh Nandakumar
+"""
+
+
+# Evaluate using Cross Validation
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+filename = 'C:/Users/Sudheesh Nandakumar/Desktop/Data Science/Python_for Data Science/pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(filename, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+
+num_folds = 10
+seed = 7
+
+kfold = KFold(n_splits=num_folds, random_state=seed)
+model = LogisticRegression(max_iter=500)
+
+results = cross_val_score(model, X, Y, cv=kfold)
+
+
+#Print results
+
+results.mean()*100.0
+results.std()*100.0
